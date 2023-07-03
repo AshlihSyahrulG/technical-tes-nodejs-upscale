987c908e125f089b7e8fa0566046add7fdef0a72
AshlihSyahrulG <ashlihsyahrulg@gmail.com>
2023-07-03 20:29:30 +0700

    feat : adding get detail endpoints

--- controllers/controller.js
+++ controllers/controller.js
@@ -12,6 +12,24 @@ class Controller{
             })
         }
     }
+    static async getTasksById (req,res){
+        const { id } = req.params
+        try {
+            const taskById = await Task.findByPk(id)
+            if(taskById){
+                res.status(200).json(taskById)
+            } else {
+                res.status(404).json({
+                    msg : 'Task not found'
+                })
+            }
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({
+                msg : "Internal server error"
+            })
+        }
+    }
 }
 
 
--- router/index.js
+++ router/index.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const Controller = require('../controllers/controller')
 
 router.get('/tasks', Controller.getTasks)
+router.get('/tasks/:id', Controller.getTasksById)
 
 module.exports = router 
